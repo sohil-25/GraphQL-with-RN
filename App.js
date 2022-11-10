70a98785b99f6ac0c7cce22f8268720f1d4a49f6
sohil-25 <sohilbakroliya2510@gmail.com>
2022-11-10 18:03:40 +0530

    intial commit

---
+++ App.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import HomeScreen from './src/HomeScreen';
+
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'https://countries.trevorblades.com/graphql',
+  cache: new InMemoryCache()
+});
+
+
+export default function App() {
+  return (
+    <ApolloProvider client={client}>
+    <View style={styles.container}>
+      <Text style={styles.title}>My Countries App</Text>
+      <HomeScreen/>
+    </View>
+    </ApolloProvider>
+  );
+}
+
+export const styles = StyleSheet.create({
+  container: {
+    paddingTop:40,
+    paddingLeft:20,
+    paddingRight: 20
+  },
+  item: {
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 20,
+    paddingRight: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  header: {
+    fontWeight: 'bold',
+    fontSize: 16
+  },
+  title:{
+    fontWeight: 'bold',
+    fontSize: 20
+  },
+  modalView: { 
+    width:'90%',
+    height:'80%',
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    paddingBottom:10
+  },
+  button: {
+    borderRadius: 10,
+    padding: 5,
+    elevation: 2,
+    backgroundColor: "lightgrey"
+  },
+});
